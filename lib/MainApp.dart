8dbadc8e41b18ac35d28a3735410a512867bdd64
Aletsfly <alex17fly@gmail.com>
2025-03-14 10:07:24 -0600

    le agrreguué un comentario a main

--- lib/MainApp.dart
+++ lib/MainApp.dart
@@ -7,6 +7,7 @@ import 'package:smn/providers/provider_lista_municipios.dart';
 import 'package:provider/provider.dart';
 import 'package:smn/providers/provider_pronosticos.dart';
 
+//aqui hare un modif para git
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -14,17 +15,21 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context)=>ProviderListaMunicipios() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
-        ),
-        ChangeNotifierProvider(create: (context)=>ProviderPronosticos() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
-        ),
-        ChangeNotifierProvider(create: (context)=>ProviderDia() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
-        ),
+        ChangeNotifierProvider(
+            create: (context) =>
+                ProviderListaMunicipios() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
+            ),
+        ChangeNotifierProvider(
+            create: (context) =>
+                ProviderPronosticos() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
+            ),
+        ChangeNotifierProvider(
+            create: (context) =>
+                ProviderDia() // este es el aviso aqui es donde se va a notificar el aviso. es la base de datos, es la vía la carretera donde recibe el aviso. el operador que recibe la peticion y checa si está
+            ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: PaginaInicial()
-      ),
+      child:
+          MaterialApp(debugShowCheckedModeBanner: false, home: PaginaInicial()),
     );
   }
 }
